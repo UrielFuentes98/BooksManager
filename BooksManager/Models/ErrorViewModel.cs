bdf4074980e0519cee6d25d1dcaa0ced4e1af532
Uriel Fuentes <urielfuentesc@gmail.com>
2021-03-22 20:23:34 -0600

    Project set up.

---
+++ BooksManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BooksManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
