34934f2850865d5632414e957c6682fe3369c1aa
Uriel Fuentes <urielfuentesc@gmail.com>
2021-03-22 22:57:03 -0600

    Implement books/records models.

---
+++ BooksManager/Models/ReadLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksManager.Models
+{
+    public class ReadLog
+    {
+        public int ReadLogId { get; set; }
+
+        [Required]
+        public int PageNumber { get; set; }
+
+        [Required]
+        public DateTime LogDate { get; set; }
+
+        public string Note { get; set; }
+
+        public int BookId { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
